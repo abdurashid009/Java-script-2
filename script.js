a9b187bca8984f5bef3617139e9cb6678154dbdf
Abdurashid <abdurashidnormetov2009.26@gmail.com>
2025-01-10 16:48:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let admin = prompt ('String typedagi malumotni kiriting');
+let pro = prompt ('Number typedagi malumotni kiriting')
+let gif = prompt ("Bolean typedagi malumotni kiriting")
+let link = confirm("Siz kiritgan malumotlar tog'riligiga ishonch hosil qiling")
+
+alert(`Siz kiritgan ${admin} sozi String typega tegishli 
+Siz kiritgan  ${pro} sozi Number typega tegishli
+Siz kiritgan  ${gif} sozi Boolean typega tegishli
+`)
+
